044483c5f229b206d7dd0ac8e5100f441fa13d0a
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2022-03-26 23:17:44 -0300

    Deixando umas partes responsivas e organizando código

--- js/geral.js
+++ js/geral.js
@@ -1,3 +1,11 @@
+function init() {
+    createList();
+    makeCarousel();
+    windowScroll();
+}
+
+init();
+
 
 function montaElemento() {
     let html = '< class="carrossel-filmes"><div class="owl-carousel owl-theme">';
@@ -13,47 +21,53 @@ function montaElemento() {
     return html;
 }
 
+function createList() {
+    $('body').after(montaElemento());
+}
+
+function makeCarousel() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+}
+
+
+function windowScroll() {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 30) {
+            $("header .container").css('background-color', 'rgba(0,0,0, .50)');
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $("header .container").css('background-color', 'rgba(0, 0, 0, .10)');
+        }
+
+        if ($(window).scrollTop() > 110) {
+            $("header .container").css('background-color', 'rgba(0,0,0, .70)');
+        }
+
+        if ($(window).scrollTop() > 150) {
+            $("header .container").css('background-color', 'rgba(0,0,0)');
+        }
+    });
+}
+
 $('.arrow').hover(function () {
     $(this).toggleClass('active-teste');
 });
 
 $('.profile img').hover(function () {
     $('.arrow').toggleClass('active-teste');
-});
-
-$('body').after(montaElemento());
-
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    responsive: {
-        0: {
-            items: 2
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
-});
-
-
-$(window).on("scroll", function () {
-    if ($(window).scrollTop() > 30) {
-        $("header .container").css('background-color', 'rgba(0,0,0, .50)');
-    } else {
-        //remove the background property so it comes transparent again (defined in your css)
-        $("header .container").css('background-color', 'rgba(0, 0, 0, .10)');
-    }
-
-    if ($(window).scrollTop() > 110) {
-        $("header .container").css('background-color', 'rgba(0,0,0, .70)');
-    }
-
-    if ($(window).scrollTop() > 150) {
-        $("header .container").css('background-color', 'rgba(0,0,0)');
-    }
 });
