0cca72a0c2accc09dc7367fb82a02a2373d35f21
joaol23 <91390174+joaol23@users.noreply.github.com>
2022-04-03 22:47:41 -0300

    Update geral.js

--- js/geral.js
+++ js/geral.js
@@ -74,7 +74,7 @@ function windowScroll() {
 async function makeListAnimes() {
     for (i = 1; i <= 8; i++) {
         var number = Math.floor(Math.random() * 3500) + 100;
-        await new Promise(r => setTimeout(r, 400));
+        await new Promise(r => setTimeout(r, 1000));
         await animes(number).then(data => { console.log(i); i != 8 ? montaLista(data) : finalizaLista(data) });
     }
 }
